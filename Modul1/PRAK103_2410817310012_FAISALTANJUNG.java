8154f903219f6a7de674be75d086178b1fb0ce5b
Faisal <kucingbau074@gmail.com>
2025-09-30 22:50:24 +0800

    Add files via upload

---
+++ Modul1/PRAK103_2410817310012_FAISALTANJUNG.java
@@ -0,0 +1,25 @@
+package Modul1;
+import java.util.Scanner;
+public class PRAK103_2410817310012_FAISALTANJUNG {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        System.out.print("");
+        int total = scanner.nextInt();
+        System.out.print("");
+        int current = scanner.nextInt();
+
+        if(current % 2 == 0){
+            current++;
+        }
+        do {
+            if (count > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(current);
+            current += 2;
+            count++;
+        } while (count < total);
+        scanner.close();
+    }
+}
